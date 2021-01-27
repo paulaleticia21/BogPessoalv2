a0244c6c0c2c3a93155f72db9027b8494dc6af3a
paulaleticia21 <paula.leticia.braz@gmail.com>
2021-01-26 23:33:23 -0300

    componente3

--- blogPessoal-front/src/app/app.component.ts
+++ blogPessoal-front/src/app/app.component.ts
@@ -1,8 +1,7 @@
-
 import { Component } from '@angular/core';
 import { AuthService } from './service/auth.service';
 
-@Component({
+@Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
